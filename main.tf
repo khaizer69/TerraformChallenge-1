7cbec89f0f5cf546a10ea8918910b24d946aeb34
junkit22 <junkit22@gmail.com>
2024-07-24 21:59:08 +0800

    update vpc name

--- main.tf
+++ main.tf
@@ -18,5 +18,10 @@ provider "aws" {
 
 #Create VPC
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "junjie-tf-vpc"
+  }
 }
