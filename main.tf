a0ab8450a14e9c06ab7635710fbe05435d03eafd
junkit22 <162791259+junkit22@users.noreply.github.com>
2024-07-24 21:23:36 +0800

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+
+#Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+  # Edit below
+  access_key = "my-access-key"
+  secret_key = "my-secret-key"
+}
+
+#Create VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
