b34740f6b421de89cad855d914dc3923abbf4e0f
junkit22 <junkit22@gmail.com>
2024-07-26 22:55:19 +0800

    add route table

--- main.tf
+++ main.tf
@@ -91,7 +91,7 @@ resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = junjie-tf-igw
+    gateway_id = aws_internet_gateway.gw.id
   }
   tags = {
     Name = "junjie-tf-public-rtb"
