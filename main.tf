32d9aa041220b3827e282685a35c6e2fdcce4521
khaizer69 <khai_razali@hotmail.com>
2024-07-27 13:12:45 +0800

    updated

--- main.tf
+++ main.tf
@@ -7,15 +7,6 @@ terraform {
   }
 }
 
-
-#Configure the AWS Provider
-provider "aws" {
-  region = "us-east-1"
-  # Edit below
-  #access_key = "my-access-key"
-  #secret_key = "my-secret-key"
-}
-
 #Create VPC
 resource "aws_vpc" "main" {
   cidr_block       = "10.0.0.0/16"
